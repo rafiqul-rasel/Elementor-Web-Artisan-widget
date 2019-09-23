f7b69a752d9f9fda0649496bdf371ae6f387a3ab
rafiqul_rasel <rafiqulcseduet@gmail.com>
2019-09-23 21:13:49 +0600

    Initial commit for my plugins

---
+++ web_artisan.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: Web Artisan
+Plugin URI: https://rafiqul.info/web_artisan
+Description: to increase your web experience
+Version: 1.0.0
+Author: Rafiqul Islam
+Author URI: https://rafiqul.info/
+Text Domain: web_artisan
+Domain Path: /lg
+*/
+
+class web_artisan
+{
+
+}
